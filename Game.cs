1c673ab9ad4c00dd301138e21fd62e4803e7a125
c-mad06 <chris.m900@icloud.com>
2025-03-09 15:10:48 +0000

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -11,7 +11,7 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
-            Room.Room("the room is a dark and damp with cold stone bricks crating the floor, walls and ceiling with nothing but a singular torch for light, as there are no windows the room is bare apart from three exits one to the north ,one east and the other west.");
+            Room.Room("the room is a dark and damp with cold stone bricks crating the floor, walls and ceiling with nothing but a singular torch for light, as there are no windows the room is bare apart from three exits one to the north ,one east and the other west and a rusted dagger on the floor.");
             Console.WriteLine("please input your name, ");
             string playerName = Console.ReadLine();
             Player.Player(playerName, 100);
