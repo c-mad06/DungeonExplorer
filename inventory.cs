fc83d550957fe7222a8eeec6a659d4b024497ab7
c-mad06 <chris.m900@icloud.com>
2025-04-30 07:44:54 +0100

    Update inventory.cs
    comments

--- inventory.cs
+++ inventory.cs
@@ -9,19 +9,24 @@ namespace DungeonExplorer
 {
     public class Inventory
     {
+        // creates lists for item inventory
         List<string> items = new List<string>();
+        // method to add items to the inventory
         public void AddItem(string item)
         {
             items.Add(item);
         }
+        // method to remove item from inventory
         public void Remove(string item)
         {
             items.Remove(item);
         }
+        // methodto return the items in the players inventory
         public string GetItems()
         {
             return string.Join(",", items);
         }
+        // sets to array then usses LNIQ to sort for the rusty dagger weapon
         public void FilterWeapons()
         {
             string[] itemsArray = items.ToArray();
@@ -33,7 +38,7 @@ namespace DungeonExplorer
                 Console.WriteLine(weapon);
             }
         }
-
+        // checks to see if the player has a weapon in their inventory and returns answer
         public bool IsWeapon()
         {
             bool weapon = false;
